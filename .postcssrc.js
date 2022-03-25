073cfaa33da83f414ebdcc1defe2383bd39874c4
iexceller <iexceller@gmail.com>
2022-03-25 20:59:23 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+// ESM
+// Common JS
+
+// import --> require()
+// export --> module.exports
+
+// const autoprefixer = requires('autoprefixer')
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
